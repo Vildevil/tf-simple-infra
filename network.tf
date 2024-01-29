af2de854854acff6b215f01245caa4f1faec137d
William <wjayberlinos@groupeais.fr>
2024-01-29 09:25:29 +0100

    Correct naming convention for subnet

--- network.tf
+++ network.tf
@@ -3,6 +3,7 @@ resource "azurerm_virtual_network" "vnet" {
   name = "vnet-${var.base_name}-01"
   address_space = var.network_configuration.address_space
   location =  data.azurerm_resource_group.rg-base.location
+  dns_servers = var.global.dns_servers
 }
 
 
@@ -11,13 +12,13 @@ resource "azurerm_subnet" "subs" {
   for_each = toset(var.network_configuration.subnets)
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name = data.azurerm_resource_group.rg-base.name
-  name = "subnet-${var.base_name}-${format("%02s", index(var.network_configuration.subnets, each.key) + 1)}"
+  name = "snet-${var.base_name}-${format("%02s", index(var.network_configuration.subnets, each.key) + 1)}"
   address_prefixes = [each.key]
 }
 
 resource "azurerm_subnet_network_security_group_association" "subnets-default-nsg" {
   for_each = azurerm_subnet.subs
 
-  subnet_id = each.value.key
-  network_security_group_id = azurerm_network_security_group.default_sg
+  subnet_id = each.value.id
+  network_security_group_id = azurerm_network_security_group.default_sg.id
 }
